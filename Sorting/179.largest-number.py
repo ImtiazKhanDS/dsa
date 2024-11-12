4f8824f49fdc799bc31ffecc91a8626a32a23188
ImtiazKhanDS <imtiaz.khan.bds@gmail.com>
2024-11-12 12:35:16 +0530

    179.largest-number

---
+++ Sorting/179.largest-number.py
@@ -0,0 +1,12 @@
+# @leet start
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = [str(num) for num in nums]
+        nums.sort(key=lambda x: x * 10, reverse=True)
+        if nums[0] == "0":
+            return "0"
+        return "".join(nums)
+
+
+# @leet end
+
