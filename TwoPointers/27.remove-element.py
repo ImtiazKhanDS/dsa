232cdf6eaeb15bdce0c6c96a0040902205e81412
ImtiazKhanDS <imtiaz.khan.bds@gmail.com>
2024-11-21 17:05:56 +0530

    27.remove-element

---
+++ TwoPointers/27.remove-element.py
@@ -0,0 +1,13 @@
+# @leet start
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for i, n in enumerate(nums):
+            if n != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
+
+
+# @leet end
+
