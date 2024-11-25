33108cf48d11789ca6e6454868fafc2d62120a57
ImtiazKhanDS <imtiaz.khan.bds@gmail.com>
2024-11-25 18:20:59 +0530

    26.remove-duplicates-from-sorted-array

---
+++ TwoPointers/26.remove-duplicates-from-sorted-array.py
@@ -0,0 +1,14 @@
+# @leet start
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 1
+        n = len(nums)
+        for j in range(1, n):
+            if nums[j] != nums[i - 1]:
+                nums[i] = nums[j]
+                i += 1
+        return i
+
+
+# @leet end
+
