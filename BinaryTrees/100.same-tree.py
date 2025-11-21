9a6c587b61c75b36c8a9cdef34465196540d509a
ImtiazKhanDS <imtiaz.khan.bds@gmail.com>
2025-11-21 11:00:14 +0530

    100.same-tree.py

---
+++ BinaryTrees/100.same-tree.py
@@ -0,0 +1,38 @@
+# @leet imports start
+from string import *
+from re import *
+from datetime import *
+from collections import *
+from heapq import *
+from bisect import *
+from copy import *
+from math import *
+from random import *
+from statistics import *
+from itertools import *
+from functools import *
+from operator import *
+from io import *
+from sys import *
+from json import *
+from builtins import *
+from typing import *
+# @leet imports end
+
+# @leet start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        
+        return p.val==q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right) 
+        
+# @leet end
